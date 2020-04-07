5b63e66d2959aa4c0e66a15a1f78793326499e95
Alex <Alex@JOHNNY5>
2020-04-07 18:31:40 -0500

    Starting point

---
+++ NameAddressForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace NameAddressForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.listCity = new System.Windows.Forms.ListBox();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.chkC = new System.Windows.Forms.CheckBox();
+            this.chkASP = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(271, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 127);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 32);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(157, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(100, 82);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(157, 20);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // listCity
+            // 
+            this.listCity.FormattingEnabled = true;
+            this.listCity.Items.AddRange(new object[] {
+            "Charlotte",
+            "Boone",
+            "Asheville"});
+            this.listCity.Location = new System.Drawing.Point(271, 266);
+            this.listCity.Name = "listCity";
+            this.listCity.Size = new System.Drawing.Size(276, 95);
+            this.listCity.TabIndex = 1;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(616, 143);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.TabIndex = 2;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(616, 166);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.TabIndex = 3;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // chkC
+            // 
+            this.chkC.AutoSize = true;
+            this.chkC.Location = new System.Drawing.Point(616, 251);
+            this.chkC.Name = "chkC";
+            this.chkC.Size = new System.Drawing.Size(39, 17);
+            this.chkC.TabIndex = 4;
+            this.chkC.Text = "c#";
+            this.chkC.UseVisualStyleBackColor = true;
+            this.chkC.CheckedChanged += new System.EventHandler(this.chkC_CheckedChanged);
+            // 
+            // chkASP
+            // 
+            this.chkASP.AutoSize = true;
+            this.chkASP.Location = new System.Drawing.Point(616, 292);
+            this.chkASP.Name = "chkASP";
+            this.chkASP.Size = new System.Drawing.Size(47, 17);
+            this.chkASP.TabIndex = 5;
+            this.chkASP.Text = "ASP";
+            this.chkASP.UseVisualStyleBackColor = true;
+            this.chkASP.CheckedChanged += new System.EventHandler(this.chkASP_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkASP);
+            this.Controls.Add(this.chkC);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.listCity);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "User Info Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listCity;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.CheckBox chkC;
+        private System.Windows.Forms.CheckBox chkASP;
+    }
+}
+
