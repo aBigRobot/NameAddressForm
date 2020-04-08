fd884e780a0f34f3b4b88abb9b9aedb75edfa55e
Alex <Alex@DELL_LAPTOP>
2020-04-07 21:34:41 -0500

    Finished Tutorial, added gender selected and city selected to message box output

--- NameAddressForm/Form1.Designer.cs
+++ NameAddressForm/Form1.Designer.cs
@@ -28,17 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Label");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Button");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Checkbox");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Root", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
             this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.listCity = new System.Windows.Forms.ListBox();
             this.radioMale = new System.Windows.Forms.RadioButton();
             this.radioFemale = new System.Windows.Forms.RadioButton();
             this.chkC = new System.Windows.Forms.CheckBox();
             this.chkASP = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -47,65 +59,76 @@
             this.groupBox1.Controls.Add(this.txtName);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(271, 111);
+            this.groupBox1.Location = new System.Drawing.Point(29, 41);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(276, 127);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(368, 156);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "User Details";
             // 
-            // label1
+            // txtAddress
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name:";
+            this.txtAddress.Location = new System.Drawing.Point(133, 105);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(208, 22);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(133, 39);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(208, 22);
+            this.txtName.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 82);
+            this.label2.Location = new System.Drawing.Point(8, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.Size = new System.Drawing.Size(99, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Address:";
             // 
-            // txtName
+            // label1
             // 
-            this.txtName.Location = new System.Drawing.Point(100, 32);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(157, 20);
-            this.txtName.TabIndex = 2;
-            // 
-            // txtAddress
-            // 
-            this.txtAddress.Location = new System.Drawing.Point(100, 82);
-            this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(157, 20);
-            this.txtAddress.TabIndex = 3;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
             // 
             // listCity
             // 
+            this.listCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listCity.FormattingEnabled = true;
+            this.listCity.ItemHeight = 29;
             this.listCity.Items.AddRange(new object[] {
             "Charlotte",
             "Boone",
             "Asheville"});
-            this.listCity.Location = new System.Drawing.Point(271, 266);
+            this.listCity.Location = new System.Drawing.Point(29, 227);
+            this.listCity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listCity.Name = "listCity";
-            this.listCity.Size = new System.Drawing.Size(276, 95);
+            this.listCity.Size = new System.Drawing.Size(368, 120);
             this.listCity.TabIndex = 1;
+            this.listCity.SelectedIndexChanged += new System.EventHandler(this.listCity_SelectedIndexChanged);
             // 
             // radioMale
             // 
             this.radioMale.AutoSize = true;
-            this.radioMale.Location = new System.Drawing.Point(616, 143);
+            this.radioMale.Location = new System.Drawing.Point(441, 80);
+            this.radioMale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioMale.Name = "radioMale";
-            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.Size = new System.Drawing.Size(59, 21);
             this.radioMale.TabIndex = 2;
             this.radioMale.TabStop = true;
             this.radioMale.Text = "Male";
@@ -114,9 +137,10 @@
             // radioFemale
             // 
             this.radioFemale.AutoSize = true;
-            this.radioFemale.Location = new System.Drawing.Point(616, 166);
+            this.radioFemale.Location = new System.Drawing.Point(441, 108);
+            this.radioFemale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioFemale.Name = "radioFemale";
-            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.Size = new System.Drawing.Size(75, 21);
             this.radioFemale.TabIndex = 3;
             this.radioFemale.TabStop = true;
             this.radioFemale.Text = "Female";
@@ -125,9 +149,10 @@
             // chkC
             // 
             this.chkC.AutoSize = true;
-            this.chkC.Location = new System.Drawing.Point(616, 251);
+            this.chkC.Location = new System.Drawing.Point(443, 198);
+            this.chkC.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkC.Name = "chkC";
-            this.chkC.Size = new System.Drawing.Size(39, 17);
+            this.chkC.Size = new System.Drawing.Size(45, 21);
             this.chkC.TabIndex = 4;
             this.chkC.Text = "c#";
             this.chkC.UseVisualStyleBackColor = true;
@@ -136,29 +161,74 @@
             // chkASP
             // 
             this.chkASP.AutoSize = true;
-            this.chkASP.Location = new System.Drawing.Point(616, 292);
+            this.chkASP.Location = new System.Drawing.Point(443, 227);
+            this.chkASP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkASP.Name = "chkASP";
-            this.chkASP.Size = new System.Drawing.Size(47, 17);
+            this.chkASP.Size = new System.Drawing.Size(57, 21);
             this.chkASP.TabIndex = 5;
             this.chkASP.Text = "ASP";
             this.chkASP.UseVisualStyleBackColor = true;
             this.chkASP.CheckedChanged += new System.EventHandler(this.chkASP_CheckedChanged);
             // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(441, 304);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(111, 43);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(607, 41);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node3";
+            treeNode1.Text = "Label";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "Button";
+            treeNode3.Name = "Node5";
+            treeNode3.Text = "Checkbox";
+            treeNode4.Name = "Node0";
+            treeNode4.Text = "Root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(243, 156);
+            this.treeView1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(607, 227);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(920, 550);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.chkASP);
             this.Controls.Add(this.chkC);
             this.Controls.Add(this.radioFemale);
             this.Controls.Add(this.radioMale);
             this.Controls.Add(this.listCity);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "User Info Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,6 +246,9 @@
         private System.Windows.Forms.RadioButton radioFemale;
         private System.Windows.Forms.CheckBox chkC;
         private System.Windows.Forms.CheckBox chkASP;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- NameAddressForm/Form1.cs
+++ NameAddressForm/Form1.cs
@@ -26,5 +26,40 @@ namespace NameAddressForm
         {
 
         }
+
+        private void listCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = listCity.GetItemText(listCity.SelectedItem);
+            MessageBox.Show(text);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string address = txtAddress.Text;
+            string city = listCity.Text;
+            string gender = string.Empty;
+            
+            //Figure out which radio button is selected
+            if (radioFemale.Checked == true)
+            {
+                gender = "Female Selected";
+            }
+            else if (radioMale.Checked == true)
+            {
+                gender = "Male Selected";
+            }
+            else
+            {
+                gender = "User did not choose a Gender! WTF?";
+            }
+
+            MessageBox.Show(name + "\n" + address + "\n" + city + "\n" + gender);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
